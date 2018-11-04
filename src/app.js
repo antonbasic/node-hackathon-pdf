2da8d2dd909fedbc80ce668ab4fc48703240b0af
David Trång <david.a.trang@gmail.com>
2018-11-04 22:18:25 +0100

    add ping endpoint and remove code that participants shall write

--- src/app.js
+++ src/app.js
@@ -4,33 +4,34 @@ const dbFactory = require('./db')
 const uuid = require('uuid/v4')
 const { generatePdfFromUrl } = require('./pdf')
 
-const db = dbFactory()
-
 const app = express()
-
 app.use(bodyParser.json())
 
-app.post('/pdf', async (req, res) => {
-  const pdf = await generatePdfFromUrl(req.body.url)
-  const key = uuid()
-  db.store(key, pdf)
+const db = dbFactory()
 
-  res.status(201).json({
-    key,
-  })
+app.post('/pdf', async (req, res, next) => {
+  const error = new Error('Not implemented')
+  error.status = 501
+  next(error)
 })
 
 app.get('/pdf/:key', (req, res, next) => {
-  res.set('Content-Type', 'application/pdf')
-  const toReturn = db.get(req.params.key)
-  if (toReturn === undefined) {
-    const error = new Error(`Document for key ${req.params.key} is not found`)
-    error.status = 404
-    return next(error)
-  }
-  res.send()
+  const error = new Error('Not implemented')
+  error.status = 501
+  next(error)
 })
 
+app.use('/ping', (req, res) => {
+  res
+    .status(200)
+    .json({
+      body: req.body,
+      queryParams: req.query,
+      path: req.path,
+    })
+})
+
+// Handle errors
 app.use((err, req, res, next) => {
   if (!err.status) {
     return res.status(500).send(err.message)
